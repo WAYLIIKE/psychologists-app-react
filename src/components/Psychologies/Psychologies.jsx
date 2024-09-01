aa0f6b0454f435a4379598a8f31e82ef92ec5e18
WAYLIIKE <valentynsavitskyy@gmail.com>
2024-09-01 19:16:19 +0200

    add pagination logic

--- src/components/Psychologies/Psychologies.jsx
+++ src/components/Psychologies/Psychologies.jsx
@@ -1,13 +1,34 @@
+import { useDispatch } from 'react-redux';
 import { PsychologiesFilters } from '../PsychologiesFilters/PsychologiesFilters';
 import { PsychologiesList } from '../PsychologiesList/PsychologiesList';
 import css from './Psychologies.module.css';
+import { fetchPsychol } from '../../redux/psychologies/psychologiesOps';
+import { useState } from 'react';
 
 export const Psychologies = () => {
+  const dispatch = useDispatch();
+  const [isLoadMore, setIsLoadMore] = useState(true);
+  let currentLimit = 6;
+
   return (
     <div className={css.container}>
       <PsychologiesFilters />
       <PsychologiesList />
-      <button className={css.button}>Load more</button>
+      {isLoadMore && (
+        <button
+          className={css.button}
+          onClick={async () => {
+            const res = await dispatch(
+              fetchPsychol({ startAt: 0, limit: currentLimit })
+            );
+            if (res.payload.length !== currentLimit)
+              return setIsLoadMore(false);
+            currentLimit += 3;
+          }}
+        >
+          Load more
+        </button>
+      )}
     </div>
   );
 };
